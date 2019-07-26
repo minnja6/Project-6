ab9e1d5d3f9ea53d5ef5540f3bd7f788603f96e0
minnja6 <jasmine.minniweather@verizon.com>
2019-07-26 14:53:55 -0500

    update
    added images and some data.json code

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require('express');
+const app = express(); 
+//const Routes = require('./routes/index');
+const data = require('./data.json');
 
-const app = express();
 
-const pug = require('pug');
 app.set('view engine', 'pug');
+
+app.use(express.static('public'));
+app.use((req, res, next) => {
+    res.locals = data;
+    next();
+});
+
+app.use('/', Routes);
+app.use('/about', Routes.about);
+app.use('/projects?', Routes.project);
+//app.use(Routes.fallback);
+app.use
 
 // app.get('/', (request, response) =>{
 //     response.send('I love treehouse!');
 // });
 
-// app.listen(3000);
+ app.listen(3000, () => console.log('App listening on port 3000'));
