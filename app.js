78362766c58031439447bcced1f6716967ad6300
minnja6 <jasmine.minniweather@verizon.com>
2019-07-29 14:18:04 -0500

    updates
    updates

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
-const express = require('express');
-const app = express(); 
-//const Routes = require('./routes/index');
-const data = require('./data.json');
-
-
+var express = require('express');
+var app = express(); 
+var data = require('./data.json');
 app.set('view engine', 'pug');
 
-app.use(express.static('public'));
-app.use((req, res, next) => {
-    res.locals = data;
-    next();
+app.get('/', (req, res) =>{
+    res.render('index', {projects:data.projects}); 
 });
 
-app.use('/', Routes);
-app.use('/about', Routes.about);
-app.use('/projects?', Routes.project);
-//app.use(Routes.fallback);
-app.use
+app.get('/about', (req, res) =>{
+     res.render('about'); 
+});
 
-// app.get('/', (request, response) =>{
-//     response.send('I love treehouse!');
-// });
+app.get('/projects', (req, res) =>{
+    res.render('project', {projects:data.projects}); 
+});
+app.use('/static', express.static('public'));
+//app.use((req, res, next) => {
+//res.locals = data;
+//next();
+ //});
 
- app.listen(3000, () => console.log('App listening on port 3000'));
+ app.listen(3000, () => {
+ console.log('App listening on port 3000')});
