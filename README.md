7076c656ecf66e41fa6e1b91ada39162e06781af
minnja6 <jasmine.minniweather@verizon.com>
2019-07-30 15:28:11 -0500

    update
    updates and finishes to all files. changed some css, ready for slack review

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Project 6
- 
+ Made changes to the buttons font family
+ Made changes to the background colors of a few elements 
+ Transformed the buttons text to uppercase when the button is hovered over
--- app.js
+++ app.js
@@ -1,16 +1,17 @@
+//requiring express and data.json, setting pug to the view engine
 var express = require('express');
 var app = express(); 
 var data = require('./data.json');
 app.set('view engine', 'pug');
-
+//setting the route to the index/home page
 app.get('/', (req, res) =>{
     res.render('index', {projects:data.projects}); 
 });
-
+//setting the route to the about page 
 app.get('/about', (req, res) =>{
      res.render('about'); 
 });
-
+//routing to the projects array and rendering the project page
 app.get('/projects/:id', (req, res) =>{
     res.render('project', {
         project: data.projects[req.params.id - 1],
@@ -19,19 +20,18 @@ app.get('/projects/:id', (req, res) =>{
 });
 app.use('/static', express.static('public'));
 
-
 //catching the error and forwarding it to the error handler
-// app.use((req, res, next) => {
-// next(createError(404));
-//  });
-//  //error handler 
-//  app.use((err, req, res, next) => {
-//     res.locals.message = err.message;
-//     res.locals.error = req.app.get('env') === 'development' ? err : {}
-//      });
-
-     //res.status(err.status || 500);
-     //res.render('error');
-
+ app.use((req, res, next) => {
+    const err=new Error("Sorry,this page doesn't exist!");
+    err.status=404;
+    next(err);
+ });
+  //error handler 
+  app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.render('error', err);
+    console.log("Sorry,this page doesn't exist!");
+      });
+//listening on port 3000
  app.listen(3000, () => {
  console.log('App listening on port 3000')});
